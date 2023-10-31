9840dfd2270523af899d4390144d3ac267dbae49
Kazushi <170826.kazushi@gmail.com>
2023-11-01 08:57:22 +0900

    Initial Commit

---
+++ MyPos/MyPosApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyPosApp.swift
+//  MyPos
+//
+//  Created by 近藤和志 on 2023/10/31.
+//
+
+import SwiftUI
+
+
+@main
+struct MyPosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
