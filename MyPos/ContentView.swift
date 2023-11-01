f54494487de8654949b155a4a1ff77590503c987
Kazushi <170826.kazushi@gmail.com>
2023-11-01 11:02:21 +0900

    Update

--- MyPos/ContentView.swift
+++ MyPos/ContentView.swift
@@ -2,44 +2,97 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var orderItems: [OrderItem] = []
+    @State private var newCustomerName = ""
+    @State private var selectedCustomerIndex = 0
+    @State private var newBentoName = ""
     @State private var selectedBentoIndex = 0
+    @State private var recordedDate: String = "None"
     @State private var newQuantity = 0
     @State private var newPrice = 0
     @State private var newTotalPrice = 0
+    @State private var newNote = ""
     
     let bentoList: [BentoItem] = [
         BentoItem(name: "ランチ", basePrice: 350),
         BentoItem(name: "お好み", basePrice: 500),
-        BentoItem(name: "唐揚げ", basePrice: 400)
+        BentoItem(name: "唐揚げ", basePrice: 400),
+        BentoItem(name: "その他", basePrice: 0)
+    ]
+    
+    let customerList: [Customer] = [
+        Customer(name: "選択してください"),
+        Customer(name: "その他"),
+        Customer(name: "日大印刷"),
+        Customer(name: "三和"),
+        Customer(name: "ジェームス"),
+        Customer(name: "おにぎりおじさん")
     ]
     
     var selectedBento: BentoItem {
         return bentoList[selectedBentoIndex]
     }
-
+    
+    var selectedCustomer: Customer {
+        return customerList[selectedCustomerIndex]
+    }
     
     var body: some View {
         Form {
             Section(header: Text("Order")) {
+                Picker("Customer", selection: $selectedCustomerIndex) {
+                    ForEach(0..<customerList.count, id: \.self) { index in
+                        Text(customerList[index].name)
+                    }
+                }
+                if selectedCustomer.name == "その他" {
+                    TextField("顧客", text: $newCustomerName)
+                }
                 Picker("弁当", selection: $selectedBentoIndex) {
                     ForEach(0..<bentoList.count, id: \.self) { index in
                         Text(bentoList[index].name)
                     }
                 }
+                if selectedBento.name == "その他" {
+                    TextField("品名", text: $newBentoName)
+                    TextField("Price", value: $newPrice, formatter: NumberFormatter())
+                }
                 TextField("Quantity", value: $newQuantity, formatter: NumberFormatter())
+                TextField("Note", text: $newNote)
                 Button("Add") {
-                    newPrice = selectedBento.basePrice
+                    let currentDate = Date()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd" // 任意の日付形式に変更可能
+                    recordedDate = dateFormatter.string(from: currentDate)
+                    if selectedCustomer.name != "その他" {
+                        newCustomerName = selectedCustomer.name
+                    }
+                    if selectedBento.name != "その他" {
+                        newBentoName = selectedBento.name
+                        newPrice = selectedBento.basePrice
+                    }
                     newTotalPrice = newPrice * newQuantity
-                    let newItem = OrderItem(name: selectedBento.name, quantity: newQuantity, price: newPrice, totalPrice: newTotalPrice)
-                    orderItems.append(newItem)
+                    let newItem = OrderItem(date: recordedDate, customer: selectedCustomer.name, name: newBentoName, quantity: newQuantity, price: newPrice, totalPrice: newTotalPrice, note: newNote)
+                    saveOrderItem(orderItem: newItem)
+                    selectedCustomerIndex = 0
+                    selectedBentoIndex = 0
+                    newCustomerName = ""
+                    newBentoName = ""
+                    newPrice = 0
                     newQuantity = 0
                     newTotalPrice = 0
+                    
                 }
             }
         }.padding()
         
         Section(header: Text("Bento Items")) {
             Table(orderItems) {
+                TableColumn("Date") { order in
+                    Text(order.date)
+                }
+                TableColumn("Customer") { order in
+                    Text(order.customer)
+                }
                 TableColumn("Name") { order in
                     Text(order.name)
                 }
@@ -52,23 +105,79 @@ struct ContentView: View {
                 TableColumn("Total") { order in
                     Text("\(order.totalPrice)")
                 }
+                TableColumn("Note") { order in
+                    Text("\(order.note)")
+                }
+                TableColumn("") { order in
+                    Button("Delete") {
+                        // 削除ボタンをタップしたときの処理を追加
+                        deleteOrderItem(order)
+                    }
+                }
             }
             
+        }.onAppear {
+            loadOrderItems()
         }
         .navigationTitle("Order")
     }
+    func saveOrderItem(orderItem: OrderItem) {
+        do {
+            if let data = UserDefaults.standard.data(forKey: "orderItems") {
+                var savedOrderItems = try JSONDecoder().decode([OrderItem].self, from: data)
+                savedOrderItems.append(orderItem)
+                if let newData = try? JSONEncoder().encode(savedOrderItems) {
+                    UserDefaults.standard.set(newData, forKey: "orderItems")
+                }
+            } else {
+                let newOrderItems = [orderItem]
+                if let newData = try? JSONEncoder().encode(newOrderItems) {
+                    UserDefaults.standard.set(newData, forKey: "orderItems")
+                }
+            }
+        } catch {
+            print("Error saving order item: \(error)")
+        }
+        loadOrderItems()
+        
+    }
+    func loadOrderItems() {
+        if let data = UserDefaults.standard.data(forKey: "orderItems") {
+            if let savedOrderItems = try? JSONDecoder().decode([OrderItem].self, from: data) {
+                orderItems = savedOrderItems
+            }
+        }
+    }
+    func deleteOrderItem(_ item: OrderItem) {
+        if let index = orderItems.firstIndex(where: { $0.id == item.id }) {
+            orderItems.remove(at: index)
+            if let newData = try? JSONEncoder().encode(orderItems) {
+                UserDefaults.standard.set(newData, forKey: "orderItems")
+            }
+            
+        }
+    }
+    
+    
 }
 
 
-struct OrderItem: Identifiable {
+struct OrderItem: Identifiable, Decodable, Encodable {
     var id = UUID()
+    var date: String
+    var customer: String
     var name: String
     var quantity: Int
     var price: Int
     var totalPrice: Int
+    var note: String
 }
 
 struct BentoItem {
     var name: String
     var basePrice: Int
+}
+
+struct Customer {
+    var name: String
 }
